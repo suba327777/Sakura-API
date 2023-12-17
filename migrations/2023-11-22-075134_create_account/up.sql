c9e00ce47ab50ddbbd8a5dc1688560be6c3951a2
suba <84484832+suba327777@users.noreply.github.com>
2023-12-17 22:42:33 +0900

    fix: typo

--- migrations/2023-11-22-075134_create_account/up.sql
+++ migrations/2023-11-22-075134_create_account/up.sql
@@ -1,9 +1,9 @@
 -- Your SQL goes here
 CREATE TABLE account (
   id bigserial NOT NULL PRIMARY KEY,
-  card_id INT REFERNCES card(id),
+  card_id INT REFERENCES card(id),
   username varchar(255) NOT NULL ,
   grade INTEGER CHECK (grade >= 1 AND grade <= 4) NOT NULL,
-  expiration_date TIMESTAMPTZ NOT NULL
+  expiration_date TIMESTAMPTZ NOT NULL,
   created_at TIMESTAMPTZ NOT NULL
 );
