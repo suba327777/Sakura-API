0159db0805539cffeadcf27216fbc9217301021c
suba <84484832+suba327777@users.noreply.github.com>
2023-12-17 22:31:20 +0900

    rm: card_type and card_number column

--- migrations/2023-11-22-075134_create_account/up.sql
+++ migrations/2023-11-22-075134_create_account/up.sql
@@ -1,9 +1,8 @@
 -- Your SQL goes here
 CREATE TABLE account (
   id bigserial NOT NULL PRIMARY KEY,
+  card_id INT REFERNCES card(id),
   username varchar(255) NOT NULL UNIQUE,
   grade INTEGER CHECK (grade >= 1 AND grade <= 4) NOT NULL,
-  card_type varchar(255) NOT NULL,
-  card_id bytea NOT NULL,
   created_at TIMESTAMPTZ NOT NULL
 );
