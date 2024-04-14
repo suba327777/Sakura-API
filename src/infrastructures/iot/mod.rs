75caab342520230f5884c54f0d0ff42dc0ec9829
niwaniwa <niwaniwa@users.noreply.github.com>
2024-04-14 18:22:55 +0900

    add mqtt subscriber

--- src/infrastructures/iot/mod.rs
+++ src/infrastructures/iot/mod.rs
@@ -2,63 +2,105 @@ use crate::domain::object::mqtt::Mqtt;
 use crate::infrastructures::config;
 use confy::load;
 use mqtt::Message;
-use paho_mqtt as mqtt;
 use std::time::Duration;
 use std::{env, process};
+
+use futures::{executor::block_on, stream::StreamExt};
+use paho_mqtt::{self as mqtt, MQTT_VERSION_5, Topic};
+use crate::domain;
 
 pub trait MessageListener: Fn(Message) + Send + Sync + 'static {}
 impl<T> MessageListener for T where T: Fn(Message) + Send + Sync + 'static {}
 
-pub fn init_mqtt() {
+pub fn init_mqtt() -> domain::object::mqtt {
     let cfg = confy::load::<config::mqtt_config::MqttConfig>("Sakura-API", None)?;
 
-    let host = env::args().nth(1).unwrap_or_else(|| cfg.host);
+    // Initialize the logger from the environment
+    env_logger::init();
 
+    let host = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "mqtt://" + cfg.address.to_string());
+
+    println!("Connecting to the MQTT server at '{}'...", host);
+
+    // Create the client. Use an ID for a persistent session.
+    // A real system should try harder to use a unique ID.
     let create_opts = mqtt::CreateOptionsBuilder::new()
         .server_uri(host)
         .client_id(cfg.device_id)
         .finalize();
 
-    let cli = mqtt::Client::new(create_opts)
-        .unwrap_or_else(|err| println!("Error creating the client: {:?}", err));
+    // Create the client connection
+    let mut cli = mqtt::AsyncClient::new(create_opts).unwrap_or_else(|e| {
+        println!("Error creating the client: {:?}", e);
+        process::exit(1);
+    });
 
-    let conn_opts = mqtt::ConnectOptionsBuilder::new()
-        .keep_alive_interval(Duration::from_secs(20))
-        .clean_session(true)
-        .finalize();
+    if let Err(err) = block_on(async {
+        // Define the set of options for the connection
+        let lwt = mqtt::Message::new(
+            "test/lwt",
+            "[LWT] Async subscriber v5 lost connection",
+            mqtt::QOS_1,
+        );
 
-    if let Err(e) = cli.connect(conn_opts) {
-        println!("Unable to connect:\n\t{:?}", e);
+        // Connect with MQTT v5 and a persistent server session (no clean start).
+        // For a persistent v5 session, we must set the Session Expiry Interval
+        // on the server. Here we set that requests will persist for an hour
+        // (3600sec) if the service disconnects or restarts.
+        let conn_opts = mqtt::ConnectOptionsBuilder::with_mqtt_version(MQTT_VERSION_5)
+            .clean_start(false)
+            .properties(mqtt::properties![mqtt::PropertyCode::SessionExpiryInterval => 3600])
+            .will_message(lwt)
+            .finalize();
+
+        cli.connect(conn_opts).await?;
+
+        let mqtt = domain::object::mqtt {
+            client: &cli,
+            config: &cfg
+        };
+
+        let _ = start_mqtt_check(mqtt);
+
+        return mqtt;
+
+    }) {
+        eprintln!("{}", err);
     }
 }
 
-pub fn subscribe_topics(
-    topic: String,
-    cli: &mqtt::Client,
-    listener: &Box<dyn MessageListener<Output = ()>>,
-) {
-    if let Err(e) = cli.subscribe_many(&topic, &[0i32]) {
-        println!("Error subscribes topics: {:?}", e);
-        process::exit(1);
+pub fn subscribe(topic: String, mqtt_client: &domain::object::mqtt){
+    let sub_opts = vec![mqtt::SubscribeOptions::with_retain_as_published(); TOPICS.len()];
+    if let Err(err) = block_on(async {
+        mqtt_client.client.subscribe_with_options(topic, 0, &sub_opts, None)
+            .await?;
+    }) {
+        eprintln!("{}", err);
     }
 
-    let handler = move |client: &mqtt::Client, msg: &Message| {
-        listener(msg.clone());
-    };
+}
 
-    // TODO: add request handler
-    match cli.subscribe(&topic.clone(), 0) {
-        Ok(mut sub_token) => {
-            if sub_token.wait().is_err() {
-                if let Some(err) = sub_token.error() {
-                    eprintln!("Error subscribing to topic {}: {:?}", topic, err);
-                    process::exit(1);
-                }
+async fn start_mqtt_check(mqtt_client: &domain::object::mqtt){
+    let mut strm = mqtt_client.client.get_stream(25);
+
+    println!("Waiting for messages...");
+    while let Some(msg_opt) = strm.next().await {
+        if let Some(msg) = msg_opt {
+            if msg.retained() {
+                print!("(R) ");
             }
+            println!("{}", msg);
         }
-        Err(e) => {
-            eprintln!("Error subscribing to topic {}: {:?}", topic, e);
-            process::exit(1);
+        else {
+            // A "None" means we were disconnected. Try to reconnect...
+            println!("Lost connection. Attempting reconnect.");
+            while let Err(err) = mqtt_client.client.reconnect().await {
+                println!("Error reconnecting: {}", err);
+                // For tokio use: tokio::time::delay_for()
+                async_std::task::sleep(Duration::from_millis(1000)).await;
+            }
         }
     }
 }
