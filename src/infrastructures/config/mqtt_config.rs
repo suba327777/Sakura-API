b3908a16aba59cc848b823ca90a9196cf4f13bed
niwaniwa <niwaniwa@users.noreply.github.com>
2024-04-14 18:22:23 +0900

    change config name

--- src/infrastructures/config/mqtt_config.rs
+++ src/infrastructures/config/mqtt_config.rs
@@ -2,14 +2,14 @@ use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct MqttConfig {
-    pub host: String,
+    pub address: String,
     pub device_id: String,
 }
 
 impl ::std::default::Default for MqttConfig {
     fn default() -> Self {
         Self {
-            host: "localhost".into(),
+            address: "localhost:8888".into(),
             device_id: "backend".into(),
         }
     }
