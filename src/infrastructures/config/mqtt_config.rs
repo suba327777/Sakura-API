50dafaabfb0d0247f49a82e7af6c6b72745acd13
niwaniwa <niwaniwa@users.noreply.github.com>
2024-04-14 20:11:48 +0900

    add mqtt handler

--- src/infrastructures/config/mqtt_config.rs
+++ src/infrastructures/config/mqtt_config.rs
@@ -1,4 +1,9 @@
+use paho_mqtt::Message;
 use serde_derive::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::Arc;
+
+type MessageHandler = Arc<dyn Fn(&Message) + Send + Sync>;
 
 #[derive(Serialize, Deserialize)]
 pub struct MqttConfig {
@@ -11,6 +16,7 @@ impl ::std::default::Default for MqttConfig {
         Self {
             address: "localhost:8888".into(),
             device_id: "backend".into(),
+            handlers: HashMap::new(),
         }
     }
 }
