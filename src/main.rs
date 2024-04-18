80a77b48ef0a3c16237378c0e2560ac976e0ae7a
niwaniwa <niwaniwa@users.noreply.github.com>
2024-04-19 01:01:52 +0900

    change start process

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 #[macro_use]
 extern crate diesel;
 
+use crate::infrastructures::config::mqtt_config::MqttConfig;
+use std::thread;
 mod domain;
 mod infrastructures;
 mod server;
 mod usecase;
 
 fn main() -> std::io::Result<()> {
-    infrastructures::iot::mqtt_client::run().expect("MQTT client initialization failed");
+    thread::spawn(move || {
+        let cfg = confy::load_path::<MqttConfig>("./config.yaml")
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+
+        let result = async move {
+            println!("mqtt start");
+            let result = infrastructures::iot::initializer::run(cfg).await;
+            println!("mqtt end");
+            result
+        };
+
+        let runtime = tokio::runtime::Runtime::new().unwrap();
+        runtime.block_on(result)
+    });
+
+    println!("server start");
     server::router::run()
 }
