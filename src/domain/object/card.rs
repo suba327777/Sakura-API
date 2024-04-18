c1dcdbcafe35fa306814d13a5bf64701ebfdfe28
niwaniwa <niwaniwa@users.noreply.github.com>
2024-04-18 19:31:04 +0900

    add objects

---
+++ src/domain/object/card.rs
@@ -0,0 +1,9 @@
+use chrono::{DateTime, Local};
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Card {
+    pub open: bool,
+    pub timestamp: DateTime<Local>,
+    pub device_id: String,
+}
