f4011bc16e3329ddb5ca8a686cc95d33cd33ec56
koke <niwaniwa@users.noreply.github.com>
2024-05-01 04:34:13 +0900

    fix class name

--- src/domain/object/mqtt/door_switch_state.rs
+++ src/domain/object/mqtt/door_switch_state.rs
@@ -2,7 +2,7 @@ use chrono::{DateTime, Local};
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct DoorState {
+pub struct DoorSwitchState {
     pub open: bool,
     pub timestamp: DateTime<Local>,
     pub device_id: String,
