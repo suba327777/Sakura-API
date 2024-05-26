046b2b28b9a9f0d9e5fd3d74e882c1cd66c648ec
suba <84484832+suba327777@users.noreply.github.com>
2024-05-26 22:50:07 +0900

    remove: account_card table

--- migrations/2023-12-17-125658_create_account_card/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE account_card ( 
-  account_id bigserial NOT NULL REFERENCES account(id),
-  card_number bytea NOT NULL,
-  PRIMARY KEY (account_id,card_number)
-);
