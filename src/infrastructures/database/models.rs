ebab5618f3d5808e34686a735f92898358f99dd0
suba <84484832+suba327777@users.noreply.github.com>
2024-05-27 00:05:14 +0900

    add: card model

--- src/infrastructures/database/models.rs
+++ src/infrastructures/database/models.rs
@@ -19,3 +19,21 @@ pub struct AccountEntity {
     pub expiration_date: NaiveDateTime,
     pub created_at: NaiveDateTime,
 }
+
+#[derive(Debug, Insertable)]
+#[table_name = "card"]
+pub struct NewCardEntity {
+    pub card_name: String,
+    pub card_number: Vec<u8>,
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Debug, Queryable, Identifiable, AsChangeset)]
+#[table_name = "card"]
+pub struct CardEntity {
+    pub id: i64,
+    pub account_id: i64,
+    pub card_name: String,
+    pub card_number: Vec<u8>,
+    pub created_at: NaiveDateTime,
+}
