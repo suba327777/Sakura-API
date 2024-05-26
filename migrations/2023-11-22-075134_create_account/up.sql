457e578ca6070a8ef25472fcc231120a24435fd0
suba <84484832+suba327777@users.noreply.github.com>
2024-05-26 22:49:40 +0900

    remove: card_id column

--- migrations/2023-11-22-075134_create_account/up.sql
+++ migrations/2023-11-22-075134_create_account/up.sql
@@ -1,7 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE account (
   id bigserial NOT NULL PRIMARY KEY,
-  card_id INT REFERENCES card(id),
   username varchar(255) NOT NULL ,
   grade INTEGER CHECK (grade >= 1 AND grade <= 4) NOT NULL,
   expiration_date TIMESTAMPTZ NOT NULL,
