ee941000524c4d098d4bc49ed430bafd19815cca
suba <84484832+suba327777@users.noreply.github.com>
2024-06-06 23:19:38 +0900

    TODO: allow dead_code

--- src/domain/repository/card.rs
+++ src/domain/repository/card.rs
@@ -7,6 +7,7 @@ pub trait CardRepository {
     fn insert(&self, card: &Card) -> anyhow::Result<()>;
     fn list(&self, account_id: &AccountId) -> anyhow::Result<Vec<Card>>;
     fn find_by_id(&self, card_id: &CardId, account_id: &AccountId) -> anyhow::Result<Card>;
+    #[allow(dead_code)]
     fn find_by_card_number(&self, card_number: &[u8]) -> anyhow::Result<bool>;
     fn delete(&self, card: &Card) -> anyhow::Result<()>;
 }
