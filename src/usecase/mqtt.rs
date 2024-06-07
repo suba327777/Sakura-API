c6733ab5cc2220dc53504ce02d25c5e69b863f7f
koke <niwaniwa@users.noreply.github.com>
2024-06-07 17:21:29 +0900

    delete initializer

--- src/usecase/mqtt.rs
+++ src/usecase/mqtt.rs
@@ -3,7 +3,6 @@ use crate::infrastructures::iot::mqtt_client::MqttClient;
 
 pub async fn run(mut client: MqttClient, cfg: MqttConfig) -> std::io::Result<()> {
     let result = client.init_mqtt().await;
-    crate::infrastructures::iot::initializer::subscribe_topics(&mut client, cfg);
     client.start_mqtt_check().await;
     result
 }
