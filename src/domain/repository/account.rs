d046593b9fdcd7c348b77909cf9126bdbf1f8774
suba <84484832+suba327777@users.noreply.github.com>
2024-06-07 16:50:58 +0900

    fix: typo

--- src/domain/repository/account.rs
+++ src/domain/repository/account.rs
@@ -5,6 +5,6 @@ pub trait AccountRepository {
     fn insert(&self, account: &Account) -> anyhow::Result<()>;
     fn list(&self) -> anyhow::Result<Vec<Account>>;
     fn find_by_id(&self, account_id: &AccountId) -> anyhow::Result<Account>;
-    fn update(&self, account: &Account) -> anyhow::anyhow::Result<()>;
+    fn update(&self, account: &Account) -> anyhow::Result<()>;
     fn delete(&self, account: &Account) -> anyhow::Result<()>;
 }
