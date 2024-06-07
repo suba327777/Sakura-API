e60f52ba40c65f1929ee027eb130895cfff90aec
suba <84484832+suba327777@users.noreply.github.com>
2024-06-07 14:58:35 +0900

    add: request model

--- src/server/request/account.rs
+++ src/server/request/account.rs
@@ -1,4 +1,4 @@
-use crate::domain::object::account::Account;
+use crate::domain::object::account::{Account, AccountId};
 use chrono::NaiveDateTime;
 use serde::Deserialize;
 
@@ -21,5 +21,14 @@ impl AccountRequest {
             self.grade.to_owned(),
             self.expiration_date.to_owned(),
         )
+    }
+    pub fn model(&self, account_id: AccountId, created_at: NaiveDateTime) -> Account {
+        Account {
+            id: account_id,
+            username: self.username.to_owned(),
+            grade: self.grade.to_owned(),
+            expiration_date: self.expiration_date.to_owned(),
+            created_at,
+        }
     }
 }
